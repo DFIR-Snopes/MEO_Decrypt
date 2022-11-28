2bf5052bca462dede8fc7b771bfc3ac69570accc
DFIR-Snopes <90684059+DFIR-Snopes@users.noreply.github.com>
2022-11-28 09:58:57 +0000

    Update MEO_Decrypt.py

--- MEO_Decrypt.py
+++ MEO_Decrypt.py
@@ -1,13 +1,22 @@
 from passlib.hash import bcrypt
 from libs import pingo
 import sys
+import os
+import datetime
 
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
 
 print("\n*************************************************")
 print("MEO DECRYPT")
 print("*************************************************")
 
-text_file = open("4_DIGIT_PINS.txt", "r")
+text_file = open(resource_path ("4_DIGIT_PINS.txt"), "r")
+
+user = os.environ.get("USERNAME")
+
+date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 PINS = text_file.read().splitlines()
 
@@ -26,4 +35,17 @@ for (index, PIN) in enumerate(PINS):
 
 if (found == 1):
     print("\n\nPIN found:", correct_PIN)
-
+else:
+    print("An error has occured, check hash is in correct format")
+    
+with open('##CHANGE##ME\Cracked.txt', 'a') as f:
+    f.write(date)
+    f.write(';')
+    f.write(user)
+    f.write(';')
+    f.write(hash)
+    f.write(';')
+    f.write(correct_PIN)
+    f.write('\n')
+    
+    input('Press Enter to Exit')
